b1fe077e4f56c4298a8d7b489183413a69bd6b71
Mykyta <honcharov.nikita70@gmail.com>
2023-08-17 22:35:03 +0200

    first commit

---
+++ Src/HW12.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class HW12 {
+    public static void main(String[] args) {
+        int[] array  = new int[]{2, 0, 5, 8, 2, 9, 4};
+        int[] array1 = new int[]{9, 3, 5, 7, 2, 8, 4};
+        for(int i = 0; i < array.length; i++){
+        for(int i1 = 0; i < array1.length; i++){
+        }
+        Arrays.sort(array);
+        Arrays.sort(array1);
+        System.out.println(Arrays.toString(array) + "Числа, які загадані фірмою-організатором лотереї");
+        System.out.println(Arrays.toString(array1) + "Числа, які вгадав гравець");
+
+
+
+        }
+    }
+}
